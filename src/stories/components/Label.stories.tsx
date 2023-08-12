53028b3a1c974fdf1973654b784f094966250312
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-08-12 17:29:31 -0300

    add Label

---
+++ src/stories/components/Label.stories.tsx
@@ -0,0 +1,15 @@
+import { Label } from "../../components";
+
+
+export default {
+    title:"UI/Label",
+    component : Label
+}
+
+const Template = () => <Label /> 
+
+export const Primary = Template.bind({});
+
+export const Secundary = Template.bind({});
+
+export const Basic = Template.bind({});
