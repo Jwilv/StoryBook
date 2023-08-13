400794d17ddb1524469c40b7fc47214e7dabf1b1
Jwilv <juan.ignacio.wilvers@gmail.com>
2023-08-13 17:41:59 -0300

    build

---
+++ storybook-static/assets/Label.stories-89bdf11a.js
@@ -0,0 +1,41 @@
+import{j as V}from"./jsx-runtime-4ca860c5.js";import"./index-61bf1805.js";import"./_commonjsHelpers-de833af9.js";const n=({label:t="no label",size:B="normal",allCaps:L=!1,colorText:k="primary",backgroundColor:q="none"})=>V.jsx("label",{style:{backgroundColor:q},className:`${B} text-${k}`,children:L?t.toUpperCase():t});try{n.displayName="Label",n.__docgenInfo={description:"",displayName:"Label",props:{label:{defaultValue:{value:"no label"},description:"this the principal Label",name:"label",required:!1,type:{name:"string"}},size:{defaultValue:{value:"normal"},description:"this the size of Label",name:"size",required:!1,type:{name:"enum",value:[{value:'"normal"'},{value:'"h1"'},{value:'"h2"'},{value:'"h3"'}]}},allCaps:{defaultValue:{value:"false"},description:"this is uppercase tag",name:"allCaps",required:!1,type:{name:"boolean"}},colorText:{defaultValue:{value:"primary"},description:"this is the color of Label",name:"colorText",required:!1,type:{name:"enum",value:[{value:'"primary"'},{value:'"secondary"'},{value:'"tertiary"'}]}},backgroundColor:{defaultValue:{value:"none"},description:"this is the background color of label",name:"backgroundColor",required:!1,type:{name:"string"}}}}}catch{}const N={title:"UI/Label",component:n,tags:["autodocs"],argTypes:{size:{control:"select"}}},e={args:{label:"Primary",size:"h1",colorText:"primary"}},a={args:{label:"Secundary",size:"h2",colorText:"secondary"}},r={args:{label:"Basic",size:"normal",colorText:"tertiary"}},o={args:{label:"Basic",size:"normal",colorText:"primary"}},s={args:{label:"Basic",size:"normal",allCaps:!0,colorText:"primary"}},l={args:{label:"Custom Font Color",size:"h1",allCaps:!0,colorText:"primary",backgroundColor:"#93bfb6"}};var c,i,p;e.parameters={...e.parameters,docs:{...(c=e.parameters)==null?void 0:c.docs,source:{originalSource:`{
+  args: {
+    label: 'Primary',
+    size: 'h1',
+    colorText: 'primary'
+  }
+}`,...(p=(i=e.parameters)==null?void 0:i.docs)==null?void 0:p.source}}};var m,u,d;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    label: 'Secundary',
+    size: 'h2',
+    colorText: 'secondary'
+  }
+}`,...(d=(u=a.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};var y,b,g;r.parameters={...r.parameters,docs:{...(y=r.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Basic',
+    size: 'normal',
+    colorText: 'tertiary'
+  }
+}`,...(g=(b=r.parameters)==null?void 0:b.docs)==null?void 0:g.source}}};var f,h,x;o.parameters={...o.parameters,docs:{...(f=o.parameters)==null?void 0:f.docs,source:{originalSource:`{
+  args: {
+    label: 'Basic',
+    size: 'normal',
+    colorText: 'primary'
+  }
+}`,...(x=(h=o.parameters)==null?void 0:h.docs)==null?void 0:x.source}}};var C,T,z;s.parameters={...s.parameters,docs:{...(C=s.parameters)==null?void 0:C.docs,source:{originalSource:`{
+  args: {
+    label: 'Basic',
+    size: 'normal',
+    allCaps: true,
+    colorText: 'primary'
+  }
+}`,...(z=(T=s.parameters)==null?void 0:T.docs)==null?void 0:z.source}}};var v,S,_;l.parameters={...l.parameters,docs:{...(v=l.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: 'Custom Font Color',
+    size: 'h1',
+    allCaps: true,
+    colorText: 'primary',
+    backgroundColor: '#93bfb6'
+  }
+}`,...(_=(S=l.parameters)==null?void 0:S.docs)==null?void 0:_.source}}};const A=["Primary","Secundary","Tertiary","Basic","AllCaps","FontColor"];export{s as AllCaps,o as Basic,l as FontColor,e as Primary,a as Secundary,r as Tertiary,A as __namedExportsOrder,N as default};
+//# sourceMappingURL=Label.stories-89bdf11a.js.map
